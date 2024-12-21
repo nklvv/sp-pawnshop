99326eb0cab93a1bb6e515013cbad40e1953803b
nklvv <162846436+nklvv@users.noreply.github.com>
2024-12-21 21:38:21 +0200

    Update sv_main.lua

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -3,34 +3,69 @@ QBCore = exports['qb-core']:GetCoreObject()
 RegisterNetEvent('sp-pawnshop:server:sellItem', function(item, price, quantity)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
+    local webhookURL = "YOUR_WEBHOOK_URL" 
 
     if not Player then return end
 
-    local itemData = Player.Functions.GetItemByName(item)
-    local webhookURL = "YOUR_WEBHOOK_URL" 
+    local itemData = nil
 
-    if itemData and itemData.amount >= quantity then
-        Player.Functions.RemoveItem(item, quantity)
-        local totalMoney = price * quantity
-        Player.Functions.AddMoney('cash', totalMoney)
+    if Config.Inventory == "qb" then
+        itemData = Player.Functions.GetItemByName(item)
+        
+        if itemData and itemData.amount >= quantity then
+            Player.Functions.RemoveItem(item, quantity)
+            
+            local totalMoney = price * quantity
+            
+            Player.Functions.AddMoney('cash', totalMoney)
+            
+            TriggerClientEvent('QBCore:Notify', src, 'You sold ' .. quantity .. 'x ' .. item .. ' for $' .. totalMoney, 'success')
 
-        TriggerClientEvent('QBCore:Notify', src, 'You sold ' .. quantity .. 'x ' .. item .. ' for $' .. totalMoney, 'success')
-
-        local discordMessage = {
-            {
-                ["color"] = tonumber("0x2b2d31"),
-                ["title"] = "Pawnshop Sale",
-                ["description"] = "**Player:** " .. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname ..
-                                  "\n**Item Sold:** " .. quantity .. "x " .. item ..
-                                  "\n**Total Received:** $" .. totalMoney,
-                ["footer"] = {
-                    ["text"] = os.date('%Y-%m-%d %H:%M:%S'),
+            local discordMessage = {
+                {
+                    ["color"] = tonumber("0x2b2d31"),
+                    ["title"] = "Pawnshop Sale",
+                    ["description"] = "**Player:** " .. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. 
+                                      "\n**Item Sold:** " .. quantity .. "x " .. item .. 
+                                      "\n**Total Received:** $" .. totalMoney,
+                    ["footer"] = {
+                        ["text"] = os.date('%Y-%m-%d %H:%M:%S'),
+                    }
                 }
             }
-        }
 
-        PerformHttpRequest(webhookURL, function(err, text, headers) end, 'POST', json.encode({username = "Pawnshop Logs", embeds = discordMessage}), {['Content-Type'] = 'application/json'})
-    else
-        TriggerClientEvent('QBCore:Notify', src, 'You don\'t have enough items', 'error')
+            PerformHttpRequest(webhookURL, function(err, text, headers) end, 'POST', json.encode({username = "Pawnshop Logs", embeds = discordMessage}), {['Content-Type'] = 'application/json'})
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'You don\'t have enough items', 'error')
+        end
+    elseif Config.Inventory == "ox" then
+        itemData = exports.ox_inventory:Search('count', item)
+
+        if itemData and itemData >= quantity then
+            exports.ox_inventory:RemoveItem(item, quantity)
+            
+            local totalMoney = price * quantity
+            
+            TriggerEvent("ox:server:addMoney", src, totalMoney)
+            
+            TriggerClientEvent('QBCore:Notify', src, 'You sold ' .. quantity .. 'x ' .. item .. ' for $' .. totalMoney, 'success')
+
+            local discordMessage = {
+                {
+                    ["color"] = tonumber("0x2b2d31"),
+                    ["title"] = "Pawnshop Sale",
+                    ["description"] = "**Player:** " .. Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. 
+                                      "\n**Item Sold:** " .. quantity .. "x " .. item .. 
+                                      "\n**Total Received:** $" .. totalMoney,
+                    ["footer"] = {
+                        ["text"] = os.date('%Y-%m-%d %H:%M:%S'),
+                    }
+                }
+            }
+
+            PerformHttpRequest(webhookURL, function(err, text, headers) end, 'POST', json.encode({username = "Pawnshop Logs", embeds = discordMessage}), {['Content-Type'] = 'application/json'})
+        else
+            TriggerClientEvent('QBCore:Notify', src, 'You don\'t have enough items', 'error')
+        end
     end
 end)
